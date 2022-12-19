c1a74bcc15535a794887faad770080081da46682
ilyas333 <muhammadilyas@bs.qau.edu.pk>
2022-12-19 13:26:24 +0000

    Addition to Q 1 & 2

---
+++ test_working_template.R
@@ -0,0 +1,51 @@
+# Test working. Your name
+
+## Q1
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
+
+## Q2
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
+
+## Q3
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
+
+## Q4
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
+
+## Q5
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
+
+## Q6
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
+
+## Q7
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
+
+## Q8
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
+
+## Q9
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
+
+## Q10
+
+# Please include text explanations in a comment (optional) 
+# and R code below (optional)
